18a356e3aece055a99e2328ce4310d1f409024c8
Anuroop <anuroopmallick1@gmail.com>
2024-10-07 01:07:52 +0530

    navabar enhancements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,35 +12,97 @@ const Navbar = () => {
     setClick(false);
   }
 
+  function handleResize() {
+    if (window.innerWidth > 768) {
+      setClick(false);
+    }
+  }
+
+  window.addEventListener("resize", handleResize);
+
   return (
-    <nav className="fixed top-0 left-0 right-0 z50 w-screen bg-transparent">
-      <div className="container px-6 py-4 mx-auto flex flex-col md:flex-row items-center bg-transparent justify-between text-white">
+    <nav className="fixed top-0 left-0 right-0 z-50 w-full bg-white">
+      <div className="px-6 py-4 mx-0 flex flex-col md:flex-row items-center justify-between text-black w-full">
         {/* Menu Icons  */}
-        <div className="w-full md:w-auto flex justify-between text-2xl font-medium pb-4">
+
+        <div className=" flex flex-row justify-between items-center text-2xl font-medium w-full">
           <Link to={"/home"}>U N O B R E A T H</Link>
           <div
             onClick={() => {
               handleClick();
             }}
-            className="cursor-pointer"
+            className="w-min cursor-pointer md:hidden"
           >
-            <i
-              className={`${click ? "fas fa-times" : "fas fa-bars"} md:hidden`}
+            <i className="fa-solid fa-bars"> </i>
+          </div>
+        </div>
+
+        <div
+          className={`fixed top-0 left-0 h-full w-full bg-white shadow-lg transiton-transform duration-300 ease-in-out ${
+            click ? "translate-x-0" : "-translate-x-full"
+          } `}
+        >
+          <div className="flex flex-col text-center">
+            <div className="container w-full text-2xl px-6 py-4 flex justify-end ">
+              <div
+                className="w-fit px-2 rounded-md transition-shadow duration-300 ease-in-out shadow-md hover:shadow-xl bg-gray-300 cursor-pointer"
+                onClick={() => {
+                  handleClick();
+                }}
+              >
+                <i className="fa-solid fa-times rounded-md "> </i>
+              </div>
+            </div>
+            <Link
+              to={"/home"}
+              onClick={closeMobileMenu}
+              className="py-6 transition-all duration-300 ease-in-out  hover:shadow-md hover:bg-gray-300"
             >
-              {" "}
-            </i>
+              Home
+            </Link>
+            <Link
+              to={"/products"}
+              onClick={closeMobileMenu}
+              className="py-6 transition-all duration-300 ease-in-out  hover:shadow-md hover:bg-gray-300"
+            >
+              Products
+            </Link>
+            <Link
+              to={"/services"}
+              onClick={closeMobileMenu}
+              className="py-6 transition-all duration-300 ease-in-out  hover:shadow-md hover:bg-gray-300"
+            >
+              Courses
+            </Link>
+            <Link
+              to={"/products"}
+              onClick={closeMobileMenu}
+              className="py-6 transition-all duration-300 ease-in-out  hover:shadow-md hover:bg-gray-300"
+            >
+              Products
+            </Link>
+            <Link
+              onClick={closeMobileMenu}
+              className="py-6 transition-all duration-300 ease-in-out  hover:shadow-md hover:bg-gray-300"
+            >
+              +91-7410095842
+            </Link>
           </div>
         </div>
 
         {/* Nav Links  */}
         <div
-          className={`flex flex-col space-y-4  md:flex-row md:space-x-8 md:block text-center ${
-            click ? "block" : "hidden"
-          }`}
+          className={`space-y-4 flex-row md:space-x-8 text-center hidden md:block`}
         >
-          <Link to={"/home"}>Home</Link>
-          <Link to={"/services"}>Services</Link>
-          <Link to={"/products"}>Products</Link>
+          <Link to={"/home"} onClick={closeMobileMenu}>
+            Home
+          </Link>
+          <Link to={"/services"} onClick={closeMobileMenu}>
+            Services
+          </Link>
+          <Link to={"/products"} onClick={closeMobileMenu}>
+            Products
+          </Link>
         </div>
       </div>
     </nav>
